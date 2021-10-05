6925c56a5173952ea145d58ec257aa15a8704c13
ackasaber <ackasaber@gmail.com>
2021-10-05 22:34:02 +0300

    Fixed a comment typo and specified the order for LAG.

--- Advanced SQL Puzzles/12-workflow-executions.sql
+++ Advanced SQL Puzzles/12-workflow-executions.sql
@@ -1,7 +1,8 @@
 WITH days_between_execs AS
     (SELECT workflow, execution_date,
             DATEDIFF(execution_date,
-                     LAG(execution_date) OVER (PARTITION BY workflow)) AS between_execs
+                     LAG(execution_date) OVER (PARTITION BY workflow
+                                               ORDER BY execution_date)) AS between_execs
      FROM workflow_executions)
 SELECT workflow, AVG(between_execs) avg_between_execs
 FROM days_between_execs
